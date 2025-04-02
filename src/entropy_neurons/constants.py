2577da7f69fb5a9ed9198657b0002f7bc3154f7e
zineddine <tighidet@isir.upmc.fr>
2025-04-03 01:03:26 +0200

    update constants

--- src/entropy_neurons/constants.py
+++ src/entropy_neurons/constants.py
@@ -1,7 +1,7 @@
 PATH = "."
 MODEL_NAMES = ["gpt2-small", "Mistral-7B-v0.1", "Phi-1_5", "EleutherAI_pythia-1.4b", "Meta-Llama-3-8B"]
 SPLIT_REGEXP = r'"|\.|\n|\\n|,|\(|\[|\]|\<'
-NB_RANDOM_SAMPLES_FOR_ABLATION = 1
+NB_RANDOM_SAMPLES_FOR_ABLATION = 100
 ABLATION_ALPHA = 3  # mean -/+ alpha*std ablation
 MAX_GEN_TOKENS = 10
 NB_COUNTER_PARAMETRIC_KNOWLEDGE = 3
